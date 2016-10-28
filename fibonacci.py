520f37cecf59468df40fa115772af4cb1d7490ac
mwendwac <mwendwa@mesozi.com>
2016-10-28 17:05:38 +0300

    sixth commit

---
+++ fibonacci.py
@@ -0,0 +1,50 @@
+
+def seriesFibonnaci(n):
+	""" Function takes integer n as an input and returns a sequence of fibonnaci numbers to that point """
+	series =[]
+	for i in range(n):
+		series.append(Fibonnaci(i))
+	return series
+
+
+
+
+
+
+def Fibonnaci(n):
+    
+
+    
+    if n == 0: 
+    	return 0
+    elif n == 1: 
+    	return 1
+    else: 
+    	return Fibonnaci(n-1)+Fibonnaci(n-2)
+
+
+
+
+
+
+
+
+print seriesFibonnaci(5)
+
+
+"""
+ASYMPTOTIC ANALYIS
+
+
+series =[] ------------------------------------>BigO(1)
+	for i in range(n):------------------------->BigO(n)
+		series.append(Fibonnaci(i))------------>BigO(n^2)
+	return series ----------------------------->BigO(1)
+
+	==========>BigO(1) + BigO(n) + BigO(n^2) + BigO(1)
+
+
+Answer ===============> BigO(2^n)
+
+
+"""
